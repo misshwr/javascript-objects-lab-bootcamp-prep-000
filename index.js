434d2e5db1b19fbfdb089d0db7049b7c350f860a
Manal HW <mnleen@gmail.com>
2019-06-20 09:06:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-<<<<<<< HEAD
+
   return Object.assign({}, object, {[key]: value});
 }
 
@@ -20,8 +20,3 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   delete object[key];
   return object;
  }
-=======
-  object={ key: value}
-  return object
-}
->>>>>>> d6ecec2e88093c49b68321ae6f46c8b4ba03113e
